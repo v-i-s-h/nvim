e50bd4a954c39de94d2db53b79bdd4b202a25d43
v-i-s-h <get_vichu@yahoo.com>
2024-07-29 00:21:23 +0530

    +nvim-ufo

---
+++ lua/plugins/nvim-ufo.lua
@@ -0,0 +1,17 @@
+local M = {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+        "kevinhwang91/promise-async"
+    },
+    enabled = vim.g.os ~= "Windows",
+    lazy = false,
+    opts = {
+        provider_selector = function(bufnr, filetype, buftype)
+            return {'treesitter', 'indent'}
+        end,
+        close_fold_kinds_for_ft = { 'imports', 'comment' }
+    }
+}
+
+return M
+
