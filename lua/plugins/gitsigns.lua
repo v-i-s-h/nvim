d07731bae0c753bc01c037191fdafe08e68f50d1
Vishnu Raj <get_vichu@yahoo.com>
2024-07-28 23:03:40 +0530

    gitsigns update

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,15 +2,18 @@ local M = {
     "lewis6991/gitsigns.nvim",
     event = "BufReadPre",
     opts = {
-        current_line_blame = true,
+        -- current_line_blame = true,
+        current_line_blame = false,
         current_line_blame_opts = {
             virt_text = true,
             -- virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
             virt_text_pos = 'right_align',
             delay = 1000,
             ignore_whitespace = false,
+            virt_text_priority = 100,
         },
-        current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+        -- current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+        current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
         numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
         linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
